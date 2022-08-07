03169ff103181821006cbc5b68fb501c932fb7a4
Nastya Severina <Severina.Nastya1992@yandex.by>
2022-08-07 21:02:33 +0300

    refactor: task complete

--- src/index.js
+++ src/index.js
@@ -38,9 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    function sliceInto(expr) {
+        let result = [];
+        for (let i = 0; i < expr.length; i +=10){
+          let chunk = expr.slice(i, i+10)
+          while (chunk.startsWith(0)){
+            chunk = chunk.substr(1)
+          }
+          result.push(chunk)
+        } return result
+    };
+    let newArr = sliceInto(expr)
+    let arr = newArr.map(element => {
+        return element.replaceAll('10','.').replaceAll('11','-')
+    });
+    
+    return arr.map(el => {
+        if(el == '**********') {
+            return  el = ' ';
+        } else {
+            return el = MORSE_TABLE[el]
+        }
+    }).join('')
+};
 
 module.exports = {
     decode
 }
+
+
+console.log(decode('00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010'))
